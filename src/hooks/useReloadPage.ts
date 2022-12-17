83e1a1ea9c513c63076b1920d5ba16829465195b
ivan-aksamentov <ivan.aksamentov@gmail.com>
2022-12-17 22:23:55 +0100

    feat: clear cache when restarting the app

--- src/hooks/useReloadPage.ts
+++ src/hooks/useReloadPage.ts
@@ -3,7 +3,9 @@ import { useCallback } from 'react'
 export function useReloadPage(url?: string | URL) {
   return useCallback(() => {
     window.history.replaceState(null, '', url)
-    window.location.reload()
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    window.location.reload(true)
     // trigger React suspense forever, to display loading spinner until the page is refreshed
     throw new Promise(() => {}) // eslint-disable-line @typescript-eslint/no-throw-literal
   }, [url])
