5ba836ba364d5ff8cafcbdc195dce941614fddd1
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-01-17 06:49:01 +0100

    refactor: extend useToggle hook

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,9 @@
 import { useCallback, useState } from 'react'
 
-export function useToggle(initialState: boolean) {
+export function useToggle(initialState = false) {
   const [state, setState] = useState(initialState)
   const toggle = useCallback(() => setState((state) => !state), [])
-  return [state, toggle]
+  const enable = useCallback(() => setState(true), [])
+  const disable = useCallback(() => setState(false), [])
+  return { state, toggle, enable, disable }
 }
